28c3f328e000c5b005ec5179c1d1748f0850c2ca
shaun <shauncarr2010@hotmail.com>
2019-12-11 14:33:04 -0600

    descpription set up done

--- Description copy/server/server.js
+++ Description copy/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
-const mysql = require('mysql');
+const path = require('path')
 const bodyParser = require('body-parser');
 const port = 3004;
 const connection = require('./config');
@@ -10,7 +10,6 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(cors());
-server.use(express.static('dist'));
 
 
 connection.connect((err) => {
@@ -20,6 +19,10 @@ connection.connect((err) => {
         console.log('connection successful')
     }
 });
+
+server.get('/getDes',(req,res) => {
+    res.sendFile(path.join(`${__dirname}/../dist/bundle.js`))
+})
 
 server.get('/Description/:id', (req, res) => {
     connection.query(`SELECT * FROM information WHERE id = '${req.params.id}';`, (err, results) => {
@@ -32,17 +35,7 @@ server.get('/Description/:id', (req, res) => {
 });
 
 
-// server.get('/Description/:id', (req, res) => {
-//    console.log(req.params.id)
-//     //connection.query('SELECT * information', (error, results) => {
-//     connection.query(`SELECT * FROM information WHERE id = '${req.body.id}';`, (err, results) => {
-//         if(error) {
-//             console.log(error)
-//         } else {
-//             res.status(200).send(results)
-//         }
-//     })    
-// });
+
 
 
 
